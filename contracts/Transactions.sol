514c3cbbca1fd89278407f7dbef79da595a7f8df
cyrus311 <shadow15mr@hotmail.com>
2022-03-18 16:02:51 +0300

    contract name fix before deploy

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-contract Transaction {
+contract Transactions {
 uint256 transactionCount;
 
 event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
